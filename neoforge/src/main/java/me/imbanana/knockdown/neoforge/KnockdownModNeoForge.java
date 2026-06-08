b6fefe9fb203bbee2cfe115803a52b9c9a9da641
ImBanana <eladtairo@gmail.com>
2026-06-08 15:06:45 +0300

    Oops

--- neoforge/src/main/java/me/imbanana/knockdown/neoforge/KnockdownModNeoForge.java
+++ neoforge/src/main/java/me/imbanana/knockdown/neoforge/KnockdownModNeoForge.java
@@ -3,14 +3,10 @@ package me.imbanana.knockdown.neoforge;
 import me.imbanana.knockdown.KnockdownMod;
 import me.imbanana.knockdown.neoforge.data.NeoForgeModDataAttachment;
 import me.imbanana.knockdown.neoforge.network.NeoForgeModNetwork;
-import me.imbanana.knockdown.network.ModNetwork;
 import me.imbanana.knockdown.util.IKnockdownable;
-import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
-import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
 @Mod(KnockdownMod.MOD_ID)
 public final class KnockdownModNeoForge {
@@ -20,9 +16,11 @@ public final class KnockdownModNeoForge {
         NeoForgeModDataAttachment.register(modBus);
         NeoForgeModNetwork.registerC2S(modBus);
         NeoForgeModNetwork.registerServer();
+
+        modBus.addListener(this::playerJoinEvent);
     }
 
-    private void PlayerJoinEvent(PlayerEvent.PlayerLoggedInEvent event) {
+    private void playerJoinEvent(PlayerEvent.PlayerLoggedInEvent event) {
         ((IKnockdownable) event.getEntity()).syncTicksLeft();
     }
 }
